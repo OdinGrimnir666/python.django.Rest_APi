1f4cc79c87f58e8b79f1c082630fe317071b7cda
OdinGrimnir666 <nlingyt@gmail.com>
2022-04-26 20:52:22 +0300

    'start'

---
+++ drfsite/women/serializers.py
@@ -0,0 +1,18 @@
+
+from email.policy import default
+import io
+from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .models import Women
+
+
+
+
+class WomenSerializer(serializers.ModelSerializer):
+    user=serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model=Women
+        fields =('__all__')
+
